d2ac4df0e3336d14cb277937cf79b49fdd7e2d53
sachinda <sachinda@parkwaylabs.com>
2024-07-23 14:35:34 +0530

    reverse string by typescript decorator

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,44 @@ app.get("/", function (req: any, res: any) {
   res.send("Hello Worldd");
 });
 
-console.log("v000");
+function split() {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const orginalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      args[0] = args[0].split("");
+      orginalMethod.apply(this, args);
+    };
+  };
+}
+function reverse() {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const orginalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      args[0] = args[0].reverse();
+      orginalMethod.apply(this, args);
+    };
+  };
+}
+function join() {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const orginalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      args[0] = args[0].join("");
+      orginalMethod.apply(this, args);
+    };
+  };
+}
+
+class StringManagerClass {
+  @split()
+  @reverse()
+  @join()
+  reverseString(str: string) {
+    console.log("inside of function ", str);
+  }
+}
+
+const stringManagerClass = new StringManagerClass();
+stringManagerClass.reverseString("abc");
+
 app.listen(3000);
